5003387ed996a7c20ab8d8024650225f9f0afe1b
Alexey Kalina <alexey.kalina@outlook.com>
2016-10-12 13:16:53 +0300

    classes added to model

---
+++ InstagramKiller/InstagramKiller.Model/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramKiller.Model
+{
+    class Comment
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid PostId { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ InstagramKiller/InstagramKiller.Model/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramKiller.Model
+{
+    class User
+    {
+        public Guid Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public byte[] Avatar { get; set; }
+        public char Sex { get; set; }
+    }
+}
