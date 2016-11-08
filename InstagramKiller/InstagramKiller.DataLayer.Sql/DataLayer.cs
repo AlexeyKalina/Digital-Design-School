bc01a21941e4b5291a245c7be0280926d7d0dd78
Alexey Kalina <alexey.kalina@outlook.com>
2016-11-08 11:57:42 +0300

    logging

--- InstagramKiller/InstagramKiller.DataLayer.Sql/DataLayer.cs
+++ InstagramKiller/InstagramKiller.DataLayer.Sql/DataLayer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using InstagramKiller.Model;
 using System.Data.SqlClient;
-using InstagramKiller.Log;
+using InstagramKiller.Utils;
 
 namespace InstagramKiller.DataLayer.Sql
 {
---
+++ InstagramKiller/InstagramKiller.Utils/Logs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace InstagramKiller.Utils
+{
+    public class Logs
+    {
+        public static Logger logger = LogManager.GetCurrentClassLogger();
+    }
+}
